ded5e2aae2fc6e4df0cccd641f63050e51ea8cb3
jenakxm <jenakxm@gmail.com>
2023-10-23 22:17:46 +0900

    [chore] unversioned files

---
+++ build/resources/main/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO review (restaurant, title, content) VALUES ('한식당', '한식당이름', '내용1')
+INSERT INTO review (restaurant, title, content) VALUES ('일식당', '일식당이름', '내용2')
+INSERT INTO review (restaurant, title, content) VALUES ('중식당', '중식당이름', '내용3')
